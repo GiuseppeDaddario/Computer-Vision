263e9f9151795abb2fb5b16226d4f92eb2b1a901
lorebenucci <lore.benucci@gmail.com>
2025-07-13 17:22:03 +0200

    PDLPR few refinements

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+
 from src.YOLOV5.YOLOV5 import YOLOV5
-#from src.PDLPR.PDLPR import PDLPR
+
+
+
+## --- PDLPR --- ##
 from src.PDLPR.training import PDLPR_training
 from src.PDLPR.inference import PDLPR_inference
 
@@ -12,13 +17,13 @@ if __name__ == "__main__":
 
 
 
-#################################
-#########   DETECTION   #########
-#################################
+    #################################
+    # --------  DETECTION   --------#
+    #################################
 
 
 
-## poi inseriamo qua la detection con yolo
+    ## poi inseriamo qua la detection con yolo
 
 
 
@@ -26,21 +31,21 @@ if __name__ == "__main__":
 
 
 
-#################################
-#########  RECOGNITION  #########
-#################################
+    #################################
+    # -------  RECOGNITION  ------- #
+    #################################
 
 
-# ------ training ------ #
-
+    # ------ training ------ #
+    print("PDLPR Training ...")
     train_folder = r"C:\Users\Lorenzo\Desktop\Computer_Vision_\dataset\CCPD2019\ccpd_base"
     PDLPR_training(train_folder, batch_size=32, num_epochs=3)
 
 
 
 
-# ------ inference ------ #
+    # ------ inference ------ #
 
-
+    print("PDLPR Inference ...")
     test_folder = r"C:\Users\Lorenzo\Desktop\Computer_Vision_\dataset\CCPD2019\ccpd_weather"
     PDLPR_inference(test_folder, batch_size=64)
