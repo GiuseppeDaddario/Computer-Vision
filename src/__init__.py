1e5221b5170d42f43cce7218338c8529d06227e1
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-07-15 22:41:27 +0200

    (Fix) packages management

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-from PDLPR import PDLPR_training, PDLPR_inference
-from YOLO import YOLOv5_training, YOLOv5_inference
+from .PDLPR import PDLPR_training, PDLPR_inference
+from .YOLO import YOLOv5_training, YOLOv5_inference
 
 __all__ = ['PDLPR_training',
            'PDLPR_inference',
