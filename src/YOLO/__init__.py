d3ef763863fd2fab54e6718afdda7525671245f5
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-07-15 22:20:45 +0200

    (Add) Original YOLOv5 repo

--- src/YOLO/__init__.py
+++ src/YOLO/__init__.py
@@ -1,5 +1,8 @@
-from yolov5 import training as YOLOv5_training
-from yolov5 import inference as YOLOv5_inference
+import sys
+sys.path.append("src/YOLO/yolov5")
+
+from yolov5.train import training as YOLOv5_training
+from yolov5.detect import inference as YOLOv5_inference
 
 __all__ = ['YOLOv5_training',
            'YOLOv5_inference']
