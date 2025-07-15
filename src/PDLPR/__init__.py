ac9f9c58abc47ef006cfe34b615c317c159aa3db
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-07-15 22:49:02 +0200

    (Fix) imports

--- src/PDLPR/__init__.py
+++ src/PDLPR/__init__.py
@@ -1,5 +1,5 @@
-from training import PDLPR_training
-from inference import PDLPR_inference
+from .training import PDLPR_training
+from .inference import PDLPR_inference
 
 __all__ = ['PDLPR_training',
            'PDLPR_inference']
