0796590ea17eb4d2bd912d335bd3396c3db4f04d
Giuseppe D'Addario <daddario.2177530@studenti.uniroma1.it>
2025-07-15 23:23:41 +0200

    (Fix) imports

--- src/YOLO/__init__.py
+++ src/YOLO/__init__.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("src/YOLO/yolov5")
 
-from train import training as YOLOv5_training
-from detect import inference as YOLOv5_inference
+from train import run as YOLOv5_training
+from detect import run as YOLOv5_inference
 
 __all__ = ['YOLOv5_training',
            'YOLOv5_inference']
