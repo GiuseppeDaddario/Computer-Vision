03ed52cf55ec0b8f0f466325417da8a33224a772
Giuseppe D'Addario <gdaddari@login05.leonardo.local>
2025-07-16 22:12:03 +0200

    (Leonardo) saving changes

--- cineca/codes/training.py
+++ cineca/codes/training.py
@@ -1,14 +1,19 @@
 # Train YOLOv5s
+import sys
+from pathlib import Path
+root_dir = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(root_dir))
+
 from src import YOLOv5_training
 
 TRAINING_PATH_YOLO = "dataset/ccpd_2019.yaml"
 
-if __name__ == "main":
+if __name__=="__main__":
     
     YOLOv5_training(
         weights="yolov5s.pt",
         data=TRAINING_PATH_YOLO,
-        epochs=300,
+        epochs=3,
         batch_size=50,
         imgsz=640,
         optimizer="Adam",
