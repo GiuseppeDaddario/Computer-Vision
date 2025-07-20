07ff3afbb2d74b88acbf37f5ef9ab1345e24d864
Giuseppe D'Addario <gdaddari@login07.leonardo.local>
2025-07-20 15:54:27 +0200

    (Leonardo) adding imports

--- src/YOLO/__init__.py
+++ src/YOLO/__init__.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append("src/YOLO/yolov5")
 
-#from train import run as YOLOv5_training
-#from detect import run as YOLOv5_inference
+from train import run as YOLOv5_training
+from models.experimental import attempt_load
+from utils.general import non_max_suppression
 
-__all__ = ['YOLOv5_training',
-           'YOLOv5_inference']
+__all__ = ['YOLOv5_training', 'attempt_load', 'non_max_suppression']
